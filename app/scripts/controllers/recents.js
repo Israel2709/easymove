ddd1ff0cb38f266aba83c7cce10dbdea0a500bb5
Rafael Vázquez Andraca <Vandrac@Mac-mini-de-Rafael.local>
2016-04-15 21:27:06 -0500

    HOTFIX > Https solved by adding manual json dummy file

--- app/scripts/controllers/recents.js
+++ app/scripts/controllers/recents.js
@@ -16,7 +16,7 @@ angular.module('easyMoveApp')
       'Karma'
     ];
 
-    $scope.serviceUrlRoot = "http://jsonstub.com/"
+    $scope.serviceUrlRoot = "https://jsonstub.com/"
 
     $scope.currentView = "statesList"
 
@@ -25,28 +25,28 @@ angular.module('easyMoveApp')
     $scope.selectedEstate = {};
     $scope.estatesList = [];
 
-    $http({
-        url: $scope.serviceUrlRoot+"states",
-        method: 'GET',
-        dataType: 'json',
-        data: '',
-        headers: {
-            'Content-Type': 'application/json',
-            'JsonStub-User-Key': '4954baf8-cbe5-4f9a-8b55-0e69f36a79d6',
-            'JsonStub-Project-Key': '91d78447-999d-4a30-9592-b7b4caf3b9fc'
-        }
-    }).then(function successCallback(response) {
-    // this callback will be called asynchronously
-    // when the response is available
-        console.log(JSON.stringify(response.data, null, 4));
-        $scope.stateList = response.data.states
-  }, function errorCallback(response) {
+  //   $http({
+  //       url: $scope.serviceUrlRoot+"states",
+  //       method: 'GET',
+  //       dataType: 'json',
+  //       data: '',
+  //       headers: {
+  //           'Content-Type': 'application/json',
+  //           'JsonStub-User-Key': '4954baf8-cbe5-4f9a-8b55-0e69f36a79d6',
+  //           'JsonStub-Project-Key': '91d78447-999d-4a30-9592-b7b4caf3b9fc'
+  //       }
+  //   }).then(function successCallback(response) {
+  //   // this callback will be called asynchronously
+  //   // when the response is available
+  //       console.log(JSON.stringify(response.data, null, 4));
+  //       $scope.stateList = response.data.states
+  // }, function errorCallback(response) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
     $http.get('/scripts/dummy/states.json').success(function (data){
          $scope.stateList = data.states
     });
-  });
+  // });
 
     $scope.stateDetail = function(selectedState) {
         /*console.log(selectedState)
@@ -54,29 +54,29 @@ angular.module('easyMoveApp')
         $scope.currentView = "estatesList"
         $scope.selectedState = $scope.stateList[selectedState];
         console.log($scope.selectedState)
-        $http({
-            url: $scope.serviceUrlRoot + "estates/_id",
-            method: 'GET',
-            dataType: 'json',
-            data: '',
-            headers: {
-                'Content-Type': 'application/json',
-                'JsonStub-User-Key': '4954baf8-cbe5-4f9a-8b55-0e69f36a79d6',
-                'JsonStub-Project-Key': '91d78447-999d-4a30-9592-b7b4caf3b9fc'
-            }
-        }).then(function successCallback(response) {
-            // this callback will be called asynchronously
-            // when the response is available
-            $scope.estatesList = response.data.estates
-            console.log($scope.estatesList)
-          }, function errorCallback(response) {
+        // $http({
+        //     url: $scope.serviceUrlRoot + "estates/_id",
+        //     method: 'GET',
+        //     dataType: 'json',
+        //     data: '',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'JsonStub-User-Key': '4954baf8-cbe5-4f9a-8b55-0e69f36a79d6',
+        //         'JsonStub-Project-Key': '91d78447-999d-4a30-9592-b7b4caf3b9fc'
+        //     }
+        // }).then(function successCallback(response) {
+        //     // this callback will be called asynchronously
+        //     // when the response is available
+        //     $scope.estatesList = response.data.estates
+        //     console.log($scope.estatesList)
+        //   }, function errorCallback(response) {
             // called asynchronously if an error occurs
             // or server returns response with an error status.
             console.log("ERROR > "+JSON.stringify(response));
             $http.get('/scripts/dummy/estates.json').success(function (data){
                  $scope.estatesList = data.estates
             });
-          });
+          // });
     }
 
     $scope.estateDetail = function(selectedEstate){
